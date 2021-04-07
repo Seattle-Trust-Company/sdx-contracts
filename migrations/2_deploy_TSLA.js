ae40725ae5716dc073d8711d6b1ad7489a95ab02
johnedquinn <johnedquinn@gmail.com>
2021-04-06 20:15:07 -0400

    Make more explicit

--- migrations/2_deploy_TSLA.js
+++ migrations/2_deploy_TSLA.js
@@ -1,12 +1,13 @@
-const TeslaShare = artifacts.require('TeslaShare');
+const Tesla = artifacts.require('Tesla');
 const Proxy = artifacts.require('AdminUpgradeabilityProxy');
+
+const node_01_addr = "0x19ABe6b87285C0E93F6181c0a6A97A554b1a2D76";
 
 module.exports = async function(deployer) {
   await deployer;
-
-  await deployer.deploy(TeslaShare);
+  await deployer.deploy(Tesla);
   const proxy = await deployer.deploy(Proxy, TeslaShare.address);
-  const proxiedTeslaShare = await TeslaShare.at(proxy.address);
-  await proxy.changeAdmin("0x19ABe6b87285C0E93F6181c0a6A97A554b1a2D76");
-  await proxiedTeslaShare.initialize();
+  const proxiedTesla = await Tesla.at(proxy.address);
+  await proxy.changeAdmin(node_01_addr);
+  await proxiedTesla.initialize();
 };
