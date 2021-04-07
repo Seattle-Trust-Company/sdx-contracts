1b28704a3fe63a06b578073363da23aef0931bd3
johnedquinn <johnedquinn@gmail.com>
2021-04-06 21:12:02 -0400

    Remove AdminUpgradeabilityProxy

--- migrations/2_deploy_TSLA.js
+++ migrations/2_deploy_TSLA.js
@@ -1,13 +1,14 @@
 const Tesla = artifacts.require('Tesla');
-const Proxy = artifacts.require('AdminUpgradeabilityProxy');
-
-const node_01_addr = "0x19ABe6b87285C0E93F6181c0a6A97A554b1a2D76";
+//const Proxy = artifacts.require('AdminUpgradeabilityProxy');
+// const node_01_addr = "0x19ABe6b87285C0E93F6181c0a6A97A554b1a2D76";
+// @TODO: Deal with the proxy
 
 module.exports = async function(deployer) {
-  await deployer;
+  deployer.deploy(Tesla);
+  /*await deployer;
   await deployer.deploy(Tesla);
-  const proxy = await deployer.deploy(Proxy, TeslaShare.address);
+  const proxy = await deployer.deploy(Proxy, Tesla.address);
   const proxiedTesla = await Tesla.at(proxy.address);
   await proxy.changeAdmin(node_01_addr);
-  await proxiedTesla.initialize();
+  await proxiedTesla.initialize();*/
 };
